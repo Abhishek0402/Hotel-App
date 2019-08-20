9f91453739652de60f05ba50c7bbe68bf7713fea
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2019-08-20 23:39:27 +0530

    passport authentication added

--- models/user.js
+++ models/user.js
@@ -1,21 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+
+    //remove user name and password bcoz they are already present in passport-local-mongoose
     admin: {
         type: Boolean,
         default: false
     }
 });
 
-
+User.plugin(passportLocalMongoose); //this will automatically add user name and password  using hash and salt and also add additional methods for user authentication
 module.exports = mongoose.model('User', User);
